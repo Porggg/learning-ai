073a331d82f576f09d4fd9343b1c9dd022788f10
Porggg <aubin.alt@gmail.com>
2026-05-09 00:14:23 +0700

    add: working neural network

--- neural-networks/mnist/mnist.py
+++ neural-networks/mnist/mnist.py
@@ -9,14 +9,8 @@ def sigmoid(z):
 def sigmoid_prime(z):
     return sigmoid(z)*(1-sigmoid(z))
 
-def hadamard_product(a, b):
-    if len(a) != len(b):
-        raise ValueError(f"The two arrays should be of same size for Hadamard Product, got {len(a)} and {len(b)}")
-
-    return [x * y for x, y in zip(a, b)]
-
 def grad_cost(activation, y):
-    return []
+    return activation - y  
 
 class Network(object):
 
@@ -40,6 +34,7 @@ class Network(object):
         return output
     
     def backprop(self, x, y):
+        L = self.num_layers
         weighted_input = []
         activations = [x]
 
@@ -51,57 +46,61 @@ class Network(object):
             activations.append(a_temp)
             output = a_temp
 
-        print(weighted_input, len(weighted_input))
-        print(activations, len(activations))
-
         # compute the error in last layer
-        delta_L = hadamard_product(grad_cost(activations[-1, y]), sigmoid_prime(weighted_input[-1]))
+        delta_L = grad_cost(activations[-1], y) * sigmoid_prime(weighted_input[-1])
         
         # backpropagation of the errors
-        # not implemented for now
-        delta = []
-        
-        return [0 for _ in range(len(self.biases))], [0 for _ in range(len(self.weights))]
+        delta = [0 for _ in range(1, L + 1)] # index 0 is unused
+        delta[-1] = delta_L
+        for l in range(2, L):
+            delta[-l] = self.weights[-l+1].T @ delta[-l+1] * sigmoid_prime(weighted_input[-l])
 
-    def SGD(self, lr, batch_size, epoch_nb, training_data):
+        return [delta[i] for i in range(1, L)], [delta[i] @ activations[i-1].T for i in range(1, L)]
+
+    def SGD(self, lr, batch_size, epoch_nb, training_data, test_data):
+        print('Starting SGD...\n')
         n_training = len(training_data)
+        n_test = len(test_data)
 
         history = [(self.biases, self.weights)]
         # last batch can be smaller than the others
         for i in range(epoch_nb):
             np.random.shuffle(training_data)
             batches = [training_data[k:k+batch_size] for k in range(0, n_training, batch_size)]
-            for batch in batches:
-                # update biases and weigth using formula (20) and (21)
-                
+            for batch in batches:                
                 # conpute the sum of derivative for each w and b 
+                sum_grad_b = [np.zeros(b.shape) for b in self.biases]
+                sum_grad_w = [np.zeros(w.shape) for w in self.weights]
                 for x, y in batch:
                     grad_b, grad_w = self.backprop(x, y)
+                    sum_grad_b = [nb+dnb for nb, dnb in zip(sum_grad_b, grad_b)]
+                    sum_grad_w = [nw+dnw for nw, dnw in zip(sum_grad_w, grad_w)]
 
-                for b, i in zip(self.biases, range(len(self.biases))):
-                    self.biases[i] = b - (lr / len(batch)) * grad_b[i]
+                for b, j in zip(self.biases, range(len(self.biases))):
+                    self.biases[j] = b - (lr / len(batch)) * grad_b[j]
                 
-                for w, i in zip(self.weights, range(len(self.weights))):
-                    self.weights[i] = w - (lr / len(batch)) * grad_w[i]
+                for w, k in zip(self.weights, range(len(self.weights))):
+                    self.weights[k] = w - (lr / len(batch)) * grad_w[k]
 
                 history.append((self.biases, self.weights))
 
-            print("Epoch ", i, "/", epoch_nb, " done.")
+            print("Epoch ", i, "/", epoch_nb-1, " done:", self.evaluate(test_data), "/", n_test)
 
         return history
     
-n = Network([2, 3, 4])
+    def evaluate(self, test_data):
+        good_count = 0
+        for x, y in test_data:
+            if np.argmax(self.feedforward(x)) == y:
+                good_count += 1
+        
+        return good_count
+    
+n = Network([784, 30, 10])
 
-print(f"Number of layers: {n.num_layers}")
-print(f"Layer sizes: {n.sizes}")
-print("\nBiases:")
-for i, b in enumerate(n.biases):
-    print(f"  Layer {i+1}:\n{b}")
-print("\nWeights:")
-for i, w in enumerate(n.weights):
-    print(f"  Layer {i+1}:\n{w}")
-print("")
-
-result = n.feedforward(np.array([[0.5],[-1.2]]))
-
-n.backprop(np.array([[0.5],[-1.2]]), 0.5)
+import data_loader
+training_data, validation_data, test_data = data_loader.load_data_wrapper()
+training_data = list(training_data)
+validation_data = list(validation_data)
+test_data = list(test_data)
+n.SGD(3, 10, 30, training_data, test_data)
